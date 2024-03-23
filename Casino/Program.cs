c3ea5e046f86d3ed378cd04ca6f19eee93378391
Sergey <100339481+alozhka@users.noreply.github.com>
2024-03-23 19:33:26 +0300

    Дописал казино с учётом правок с код-ревью

---
+++ Casino/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Casino;
+
+internal class Program
+{
+    public static void Main()
+    {
+        var rand = new Random();
+        decimal balance = 10000;
+        const int multiplicator = 10;
+        
+        while (balance > 0)
+        {
+            Console.WriteLine("\nБаланс: " + balance);
+            Console.Write("Введите ставку: ");
+            bool betParseResult = int.TryParse(Console.ReadLine(), out var bet);
+            if (betParseResult is false)
+            {
+                Console.WriteLine("Введены неверные данные!");
+                continue;
+            }
+
+            if (bet > balance)
+            {
+                Console.WriteLine("Недостаточно денег для ставки!");
+            }
+            else
+            {
+                int randomNumber = rand.Next(20) + 1;
+                if (randomNumber is 18 or 19 or 20)
+                {
+                    var profit = bet * (1 + multiplicator * randomNumber % 17);
+                    balance += profit;
+                    Console.WriteLine("Выигрыш ставки: +" + profit);
+                }
+                else
+                {
+                    balance -= bet;
+                    Console.WriteLine("Проигрыш ставки.");
+                }
+            }
+        }
+
+        Console.WriteLine("Деньги на счёте истрачены!");
+    }
+}
