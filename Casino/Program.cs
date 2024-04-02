6ce899451bd10184acd27ed96b64fcd22b45cca1
Sergey <100339481+alozhka@users.noreply.github.com>
2024-04-02 22:28:33 +0300

    Обновил приложение со всеми доработками
    Вынес выйгрышные значения в константное поле, убрал var, сделал более понтные названия переменных

--- Casino/Program.cs
+++ Casino/Program.cs
@@ -1,19 +1,20 @@
 ﻿namespace Casino;
 
-internal class Program
+internal static class Program
 {
+    private const int Multiplicator = 10;
+    private static readonly IEnumerable<int> WinningValues = new[] { 18, 19, 20 };
     public static void Main()
     {
-        var rand = new Random();
+        Random rand = new Random();
         decimal balance = 10000;
-        const int multiplicator = 10;
         
         while (balance > 0)
         {
             Console.WriteLine("\nБаланс: " + balance);
             Console.Write("Введите ставку: ");
-            bool betParseResult = int.TryParse(Console.ReadLine(), out var bet);
-            if (betParseResult is false)
+            bool isValidBet = int.TryParse(Console.ReadLine(), out int bet);
+            if (!isValidBet)
             {
                 Console.WriteLine("Введены неверные данные!");
                 continue;
@@ -26,11 +27,11 @@ internal class Program
             else
             {
                 int randomNumber = rand.Next(20) + 1;
-                if (randomNumber is 18 or 19 or 20)
+                if (WinningValues.Contains(randomNumber))
                 {
-                    var profit = bet * (1 + multiplicator * randomNumber % 17);
+                    int profit = bet * (1 + Multiplicator * randomNumber % 17);
                     balance += profit;
-                    Console.WriteLine("Выигрыш ставки: +" + profit);
+                    Console.WriteLine($"Выигрыш ставки: {profit}");
                 }
                 else
                 {
