c8934ba77692aa66857e97e7b874103092f851aa
Sergey <100339481+alozhka@users.noreply.github.com>
2024-05-07 17:50:08 +0300

    Сделал 1 задание, написал для 2 задания вариант без вложенных объектов

--- lab-tools/src/actions.ts
+++ lab-tools/src/actions.ts
@@ -3,26 +3,60 @@ import parse, { HTMLElement } from 'node-html-parser'
 
 const handleHtmlParse = (fileName: string): void => {
 	try {
-		const document: string = fs.readFileSync(fileName, {encoding: 'utf8'})
-		const nodes = parse(document).querySelectorAll("[href],[src]")
+		const rawDocument: string = fs.readFileSync(fileName, { encoding: 'utf8' })
+		const nodes: HTMLElement[] = parse(rawDocument).querySelectorAll('[href],[src]')
 		const hrefsSet = new Set()
-		
-		for(const node of nodes) {
+
+		for (const node of nodes) {
 			hrefsSet.add(node.attributes.href)
 			hrefsSet.add(node.attributes.src)
 		}
 		hrefsSet.delete(undefined)
-		
+
 		console.log(Array.from(hrefsSet))
-	} catch (e) {
-		if (typeof e === 'object' && e !== null && 'message' in e) {
-			console.error(e.message)
-		} else {
-			console.error("An error happened")
-		}
+	} catch (e: unknown) {
+		handleError(e)
 	}
 }
 
+type JsonParseData = Record<string, string>
+type NodeStatus = 'unchanged' | 'changed' | 'new' | 'deleted'
+type Nodes = Record<string, NodeStatus>
 
+const handleJsonDifference = (firstJsonFileName: string, secondJsonFileName: string): void => {
+	try {
+		let firstJson: JsonParseData = JSON.parse(fs.readFileSync(firstJsonFileName, { encoding: 'utf8' }))
+		let secondJson: JsonParseData = JSON.parse(fs.readFileSync(secondJsonFileName, { encoding: 'utf8' }))
 
-export { handleHtmlParse }
+		const jsonDiff: Nodes = {}
+		for (const key in firstJson) {
+			if (firstJson[key] === secondJson[key])
+				jsonDiff[key] = 'unchanged'
+
+			if (firstJson[key] !== secondJson[key])
+				jsonDiff[key] = 'changed'
+
+			if (!(key in secondJson))
+				jsonDiff[key] = 'deleted'
+		}
+
+		for (const key in secondJson) {
+			if (!(key in jsonDiff))
+				jsonDiff[key] = 'new'
+		}
+
+		console.log(jsonDiff)
+	} catch (e: unknown) {
+		handleError(e)
+	}
+}
+
+const handleError = (e: unknown): void => {
+	if (typeof e === 'object' && e !== null && 'message' in e) {
+		console.error(e.message)
+	} else {
+		console.error('An error happened')
+	}
+}
+
+export { handleHtmlParse, handleJsonDifference }
--- lab-tools/src/lab-tools.ts
+++ lab-tools/src/lab-tools.ts
@@ -1,11 +1,16 @@
 import {Command} from "commander";
-import {handleHtmlParse} from "./actions";
+import { handleHtmlParse, handleJsonDifference } from './actions'
 
 const program = new Command();
 
 program.command('html-resources')
     .argument('<fileName>', 'a file name to parse')
     .action(handleHtmlParse)
+
+program.command('json-diff')
+	.argument('<firstJsonFileName>', 'old json file')
+	.argument('<secondJsonFileName>', 'new json file')
+	.action(handleJsonDifference)
 
 
 program.parse()
