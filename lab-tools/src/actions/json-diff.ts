49426e59d63497bf78c0568e03414e416cd536bf
Sergey <100339481+alozhka@users.noreply.github.com>
2024-05-13 19:58:37 +0300

    Остался баг с массивами

---
+++ lab-tools/src/actions/json-diff.ts
@@ -0,0 +1,197 @@
+import fs from 'node:fs'
+
+
+type JsonParseData = Record<string, unknown>
+
+const findFilesJsonDifference = (firstJsonFileName: string, secondJsonFileName: string): Differences => {
+		const firstJson: JsonParseData = JSON.parse(fs.readFileSync(firstJsonFileName, { encoding: 'utf8' }))
+		const secondJson: JsonParseData = JSON.parse(fs.readFileSync(secondJsonFileName, { encoding: 'utf8' }))
+
+
+		return findJsonDifference(firstJson, secondJson)
+}
+
+type DifferenceType = 'new' | 'delete' | 'unchanged' | 'changed';
+type Difference = {
+	type: DifferenceType
+	newValue?: unknown
+	oldValue?: unknown
+	children?: Differences
+}
+type ArrayDifference = {
+	type: DifferenceType
+	children: Differences
+}
+type Differences = Record<string, Difference>
+
+//TODO: переписать с any на unknown
+function findJsonDifference(oldJson: any, newJson: any): Record<string, Difference> {
+	const differences: Record<string, Difference> = {}
+
+	if (typeof oldJson !== 'object' || typeof newJson !== 'object') {
+		throw new Error('Invalid JSON')
+	}
+
+	for (const key in oldJson) {
+		if (oldJson.hasOwnProperty(key)) {
+			if (!newJson.hasOwnProperty(key)) {
+				differences[key] = {
+					type: 'delete',
+					oldValue: oldJson[key]
+				}
+			} else if (areObjects(oldJson[key], newJson[key])) {
+				// если простые объекты или простые массивы
+				if (areSimpleObjects(oldJson[key], newJson[key])) {
+					// если оба - массивы
+					if (everyHasArray(oldJson[key], newJson[key])) {
+						differences[key] = findArrayDifference(oldJson[key],  newJson[key])
+					} else {
+						// либо оба - простые объекты, либо один из них - массив
+						differences[key] = {
+							type: oldJson[key] === newJson[key] ? 'unchanged' : 'changed',
+							oldValue: oldJson[key],
+							newValue: newJson[key]
+						}
+					}
+				}
+				// если сложный объект
+				else {
+					differences[key] = {
+						type: oldJson[key] === newJson[key] ? 'unchanged' : 'changed',
+						children: findJsonDifference(oldJson[key], newJson[key])
+					}
+				}
+				// единый тип, либо разный
+			} else if (oldJson[key] !== newJson[key]) {
+				differences[key] = {
+					type: 'changed',
+					oldValue: oldJson[key],
+					newValue: newJson[key]
+				}
+				// единый тип 
+			} else {
+				differences[key] = {
+					type: 'unchanged',
+					oldValue: oldJson[key],
+					newValue: newJson[key]
+				}
+			}
+		}
+	}
+
+	for (const key in newJson) {
+		if (newJson.hasOwnProperty(key) && !oldJson.hasOwnProperty(key)) {
+			differences[key] = {
+				type: 'new',
+				newValue: newJson[key]
+			}
+		}
+	}
+	return differences
+}
+
+const everyHasArray = (...objects: object[]): boolean => {
+	for (const object of objects) {
+		if (!Array.isArray(Object.values(object)[0])) {
+			return false
+		}
+	}
+	
+	return true
+}
+
+const areObjects = (...list: any[]): boolean => {
+	for (const el of list) {
+		if (typeof el !== 'object') {
+			return false
+		}
+	}
+	
+	return true;
+}
+
+const areSimpleObjects = (...objects: object[]): boolean => {
+	for (const obj of objects) {
+		if (Object.keys(obj).length !== 1) return false
+	}
+	return true
+}
+
+
+//TODO: дописать
+/*
+const findObjectDifference = (oldObj: object,  newObj: object): Difference => {
+	let diff: Difference;
+	
+	// если простые объекты или простые массивы
+	if (areSimpleObjects(oldObj, newObj)) {
+		// если оба - массивы
+		for (const obj of oldObj) {
+			
+		}
+		if () {
+			diff = findArrayDifference(oldObj, newObj)
+		} else {
+			// либо оба - простые объекты, либо один из них - массив
+			diff = {
+				type: oldObj === newObj ? 'unchanged' : 'changed',
+				oldValue: oldObj,
+				newValue: newObj
+			}
+		}
+	}
+	// если сложный объект
+	else {
+		diff = {
+			type: oldObj === newObj ? 'unchanged' : 'changed',
+			children: findJsonDifference(oldObj, newObj)
+		}
+	}
+	
+	return diff
+}
+*/
+
+
+const findArrayDifference = (firstObj: Record<string, unknown[]>, secondObj: Record<string, unknown[]>): Difference => {
+	const diff: ArrayDifference = {
+		type: 'unchanged',
+		children: {}
+	}
+	const first: unknown[] = Object.values(firstObj)[0]
+	const second: unknown[] = Object.values(secondObj)[0]
+	
+	
+	const maxLength = Math.max(first.length, second.length)
+	for (let i = 0; i < maxLength; i++) {
+		const oldVal = first[i]
+		const newVal = second[i]
+		
+		let type: DifferenceType;
+		if (oldVal === undefined) {
+			diff.children[i.toString()] = {
+				type: 'new',
+				newValue: newVal
+			}
+		}
+		else if (newVal === undefined) {
+			diff.children[i.toString()] = {
+				type: 'delete',
+				oldValue: oldVal
+			}
+		}
+		else 
+			diff.children[i.toString()] = {
+				type: oldVal === newVal ? 'unchanged' : 'changed',
+				oldValue: oldVal,
+				newValue: newVal
+			}
+
+		
+	}
+	
+	return diff
+}
+
+
+export default findFilesJsonDifference
--- lab-tools/src/lab-tools.ts
+++ lab-tools/src/lab-tools.ts
@@ -1,16 +1,28 @@
-import {Command} from "commander";
-import { handleHtmlParse, handleJsonDifference } from './actions'
+import { Command } from 'commander'
 
-const program = new Command();
+import parseHtml from './actions/html-parser'
+import findFilesJsonDifference from './actions/json-diff'
+
+const program = new Command()
 
 program.command('html-resources')
-    .argument('<fileName>', 'a file name to parse')
-    .action(handleHtmlParse)
+	.argument('<fileName>', 'a file name to parse')
+	.action((fileName: string) => {
+		const result: string[] = parseHtml(fileName)
+		console.log(result)
+	})
 
 program.command('json-diff')
 	.argument('<firstJsonFileName>', 'old json file')
 	.argument('<secondJsonFileName>', 'new json file')
-	.action(handleJsonDifference)
+	.action((firstJsonFileName: string, secondJsonFileName: string)=> {
+		const result: Record<string, object> = findFilesJsonDifference(firstJsonFileName, secondJsonFileName)
+		console.log(JSON.stringify(result, null, 2));
+	})
 
 
-program.parse()
+try {
+	program.parse()
+} catch (e) {
+	console.error(e)
+}
