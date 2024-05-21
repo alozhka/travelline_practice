c5afdbad9544d89dff80151d44e245572b89ee11
Sergey <100339481+alozhka@users.noreply.github.com>
2024-05-21 14:39:46 +0300

    Сделал запросы

--- baby-swagger/Frontend/ClientApp/src/components/control-buttons/controlButtons.ts
+++ baby-swagger/Frontend/ClientApp/src/components/control-buttons/controlButtons.ts
@@ -13,22 +13,26 @@ const createControlButtons = (path: string, method: string): string => {
 }
 
 const addExecutionListener = (el: HTMLElement, endpoint: Endpoint) => {
-  const requestBody = document.getElementById(`requestBody!${endpoint.path}!${endpoint.method}`) as HTMLPreElement
+  const requestBody = document.getElementById(`requestBody!${endpoint.path}!${endpoint.method}`) as (HTMLPreElement | undefined)
   const responseArea = document.getElementById(`responseArea!${endpoint.path}!${endpoint.method}`) as HTMLDivElement
-  
+
   const executeButton = el.getElementsByClassName('control-buttons-execute')[0]
-  executeButton.addEventListener('click', (e: Event) => {
+  executeButton.addEventListener('click', async (e: Event) => {
     const buttonsArea = (e.currentTarget as HTMLButtonElement).parentElement || undefined
     buttonsArea?.classList.add('control-buttons-expand')
-    responseArea.classList.remove("hidden")
+    responseArea.classList.remove('hidden')
+    
+    const body = requestBody ? requestBody.innerHTML : null
+    const response = await executeRequest(endpoint.path, endpoint.method, body)
+    renderResponse(responseArea, response)
   })
-  
+
   const clearButton = el.getElementsByClassName('control-buttons-clear')[0]
-  
+
   clearButton.addEventListener('click', (e: Event) => {
     const buttonsArea = (e.currentTarget as HTMLButtonElement).parentElement || undefined
     buttonsArea?.classList.remove('control-buttons-expand')
-    responseArea.classList.add("hidden")
+    responseArea.classList.add('hidden')
   })
   if (endpoint.requestBody) {
     const props = endpoint.requestBody.properties
@@ -36,8 +40,30 @@ const addExecutionListener = (el: HTMLElement, endpoint: Endpoint) => {
       requestBody.innerHTML = JSON.stringify(propertiesToObject(props), null, 2)
     })
   }
+
+}
+
+const executeRequest = async (path: string, method: string, body: string | null): Promise<Response> => {
+  return fetch(`http://localhost:5154${path}`, {
+    method,
+    body
+  })
 }
 
 
-export {addExecutionListener }
-export default createControlButtons;
+const renderResponse = async (responseArea: HTMLDivElement, response: Response): Promise<void> => {
+  const wrappers = Array.from(responseArea.getElementsByTagName('code'));
+  let headers = ''
+  response.headers.forEach((value:string, key: string) => {
+    headers += `${key}: ${value}\n`
+  })
+  
+  wrappers[0].innerText = response.url
+  wrappers[1].innerText = `${response.status} ${response.statusText}`
+  wrappers[2].innerText = JSON.stringify(await response.json(), null, 2)
+  wrappers[3].innerText = headers
+}
+
+
+export { addExecutionListener }
+export default createControlButtons
