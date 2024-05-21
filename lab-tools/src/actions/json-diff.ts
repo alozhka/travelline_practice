5f91930ceb9288948a64d336003ce0daa6070e13
Sergey <100339481+alozhka@users.noreply.github.com>
2024-05-21 14:01:09 +0300

    Убрал мутации из алгоритма

--- lab-tools/src/actions/json-diff.ts
+++ lab-tools/src/actions/json-diff.ts
@@ -13,53 +13,59 @@ const findFilesJsonDifference = (firstJsonFileName: string, secondJsonFileName: 
 type DifferenceType = 'new' | 'delete' | 'unchanged' | 'changed';
 type Difference = {
 	type: DifferenceType
-	newValue?: unknown
 	oldValue?: unknown
+	newValue?: unknown
 	children?: Differences
-}
-type ArrayDifference = {
-	type: DifferenceType
-	children: Differences
 }
 type Differences = Record<string, Difference>
 
 
 const isObject = (value: unknown): value is Record<string, unknown> => typeof value === 'object' && value !== null
 
-function findJsonDifference(oldJson: Record<string, unknown>, newJson: Record<string, unknown>): Record<string, Difference> {
-	const differences: Record<string, Difference> = {}
-	const keys = [...Object.keys(oldJson), ...Object.keys(newJson)] 
+function findJsonDifference(oldJson: Record<string, unknown>, newJson: Record<string, unknown>): Differences {
+	const keys: string[] = [...Object.keys(oldJson), ...Object.keys(newJson)] 
 	
-	for (const key of keys) {
-		const oldValue = oldJson[key]
+	
+	return keys.reduce<Differences>((differences: Differences, key: string) => {
+		const oldValue = oldJson[key];
 		const newValue = newJson[key]
 		
-		if (oldValue === undefined) {
-			differences[key] = {
-				type: 'new',
-				newValue: newValue
+		if (oldValue === undefined) 
+			return {
+				...differences,
+				[key]: {
+					type: 'new',
+					newValue
+				}
 			}
-		} else if (newValue === undefined) {
-			differences[key] = {
-				type: 'delete',
-				oldValue: oldValue
+		
+		if (newValue === undefined) 
+			return {
+				...differences,
+				[key]: {
+					type: 'delete',
+					oldValue
+				}
 			}
-		} else if (isObject(oldValue) && isObject(newValue)) {
-			differences[key] = {
-				type: JSON.stringify(oldValue) === JSON.stringify(newValue) ? 'unchanged' : 'changed',
-				children: findJsonDifference(oldValue, newValue)
+		
+		if (isObject(oldValue) && isObject(newValue)) 
+			return {
+				...differences,
+				[key]: {
+					type: JSON.stringify(oldValue) === JSON.stringify(newValue) ? 'unchanged' : 'changed',
+					children: findJsonDifference(oldValue, newValue)
+				}
+			}
+		
+		return {
+			...differences,
+			[key]: {
+				type: oldValue === newValue ? 'unchanged' : 'changed',
+				oldValue,
+				newValue
 			}
 		}
-		else {
-			differences[key] = {
-				type: oldJson[key] === newJson[key] ? 'unchanged' : 'changed',
-				oldValue: oldValue,
-				newValue: newValue
-			}
-		}
-	}
-	
-	return differences
+	}, {})
 }
 
 
