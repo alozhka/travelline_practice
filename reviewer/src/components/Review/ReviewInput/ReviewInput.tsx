8ea2f0973532b23f68f404d5cbd0fbc749d9fe22
Sergey <100339481+alozhka@users.noreply.github.com>
2024-05-25 22:12:08 +0300

    Доделал изменение статус бара в инпуте

--- reviewer/src/components/Review/ReviewInput/ReviewInput.tsx
+++ reviewer/src/components/Review/ReviewInput/ReviewInput.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useRef } from 'react'
+import React, { forwardRef, useEffect, useRef } from 'react'
 
 import s from './ReviewInput.module.css'
 import angryFace from './images/twemoji_angry-face.svg'
@@ -6,18 +6,21 @@ import slightlyFrowningFace from './images/twemoji_slightly-frowning-face.svg'
 import neutralFace from './images/twemoji_neutral-face.svg'
 import slightlySmilingFace from './images/twemoji_slightly-smiling-face.svg'
 import grinningFace from './images/twemoji_grinning-face-with-big-eyes.svg'
+import { Simulate } from 'react-dom/test-utils'
+import compositionStart = Simulate.compositionStart
 
 type RangeState = {
 	color: string,
 	emojiUrl: string
 }
 
+//TODO: переделать на хештеги из фигмы
 const rangeStates: RangeState[] = [
 	{ color: 'red', emojiUrl: angryFace },
-	{ color: 'orange', emojiUrl: slightlyFrowningFace },
-	{ color: 'lightorange', emojiUrl: neutralFace },
-	{ color: 'yellow', emojiUrl: slightlySmilingFace },
-	{ color: 'lightyellow', emojiUrl: grinningFace }
+	{ color: 'orangered', emojiUrl: slightlyFrowningFace },
+	{ color: 'darkorange', emojiUrl: neutralFace },
+	{ color: 'orange', emojiUrl: slightlySmilingFace },
+	{ color: 'yellow', emojiUrl: grinningFace }
 ]
 
 
@@ -27,15 +30,23 @@ interface ReviewInputProps {
 
 const ReviewInput = forwardRef((props: ReviewInputProps, ref: React.ForwardedRef<HTMLInputElement>) => {
 	const statusBarRef = useRef<HTMLDivElement>(null)
+	const closingBarRef = useRef<HTMLDivElement>(null)
+	
 	const handleChange = () => {
-		if (statusBarRef.current && ref.current !== null)
-		statusBarRef.current.style.backgroundColor = rangeStates[ref.current.value].color
+		if (statusBarRef.current && closingBarRef.current) {
+			const index = ref.current.valueAsNumber - 1
+			statusBarRef.current.style.backgroundColor = rangeStates[index].color
+			closingBarRef.current.style.width = `${100 - 25 * index}%`
+			closingBarRef.current.style.marginLeft = `${100 - 25 * (4 - index)}%`
+		}
 	}
+	useEffect(() => handleChange())
 	return (
 		<div className={s.wrapper}>
 			<input ref={ref} type="range" min="1" max="5" step="1" 
 				onChangeCapture={handleChange} name={props.category}/>
 			<div ref={statusBarRef} className={s.statusBar}></div>
+			<div ref={closingBarRef} className={`${s.statusBar} ${s.closingBar}`}></div>
 			<div className={s.rangePoints}>
 				{ rangeStates.map((range, index) => 
 					<div key={`range${props.category}${index}`} className={s.rangePoint} style={{
